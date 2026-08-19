801cc75edb96d1f2057206ad2d480619259eef67
Adlan Daniel <68151150+adaniel22@users.noreply.github.com>
2026-08-19 21:16:42 +0200

    Gateway: CORS engedelyezese a frontendnek

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173',
+  });
   const config = app.get(ConfigService);
 
   const authUrl = config.get<string>('AUTH_SERVICE_URL')!;
